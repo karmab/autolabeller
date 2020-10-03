ac03a7778c50691dd4c140259ec71aa10e229849
karmab <karimboumedhel@gmail.com>
2020-10-03 21:08:38 +0200

    fix read

--- autosigner.py
+++ autosigner.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     certs_api = client.CertificatesV1beta1Api()
     try:
         k8sfile = '/var/run/secrets/kubernetes.io/serviceaccount/namespace'
-        namespace = os.open(k8sfile).read() if os.path.exists(k8sfile) else os.environ.get('NAMESPACE', 'default')
+        namespace = os.open(k8sfile, 'r').read() if os.path.exists(k8sfile) else os.environ.get('NAMESPACE', 'default')
         config_map_name = os.environ.get('CONFIG_MAP', 'autorules')
         config_map = v1.read_namespaced_config_map(namespace=namespace, name=config_map_name)
         config_map_data = config_map.to_dict().get('data', {})
